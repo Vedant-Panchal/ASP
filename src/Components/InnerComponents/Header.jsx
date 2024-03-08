c34a2ea6234e680688c2865fab65820d1dff619a
Mrunal Shah <shahmrunal777@gmail.com>
2024-03-08 21:05:03 +0530

    Modified APK URL on Header.

--- src/Components/InnerComponents/Header.jsx
+++ src/Components/InnerComponents/Header.jsx
@@ -76,14 +76,14 @@ function Header() {
             </button>
 
             <Link
-              to={"/"}
+              to={"https://github.com/XENOSTAR7/ASP/releases/download/v1.0.0/ASP-v1.0.0-arm64-v8a-release.apk"}
               className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 transition-all duration-200 ease-in"
-              onClick={() =>
+             /* onClick={() =>
                 window.open(
-                  "https://github.com/XENOSTAR7/ASP/releases/download/v1.0.0/ASP-v1.0.0-arm64-v8a-release.apk",
-                  "_blank"
+                  "https://github.com/XENOSTAR7/ASP/releases/download/v1.0.0/ASP-v1.0.0-arm64-v8a-release.apk"
+                
                 )
-              }
+              }*/
             >
                Download APP  
             </Link>
